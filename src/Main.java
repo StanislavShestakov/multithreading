c68c27cbd993ce20b5ffc56d40719a015264940e
admin <w3orks@mail.ru>
2021-05-20 13:38:49 +0300

    wlile 100 ms

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,23 @@
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-//        MyThread myThread = new MyThread();
-//        myThread.start();
-//
-//        MyThread myThread2 = new MyThread();
-//        myThread2.start();
 
-        Thread thread = new Thread(new Runner());
-        thread.start();
-        System.out.println("Hello from main thread!");
+        MyThread myThread = new MyThread();
+        myThread.start();
     }
 }
 
-class Runner implements Runnable{
-    @Override
-    public void run() {
-        for (int i = 0; i <1000 ; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Hello My Thread!" + i);
-        }
-    }
-}
+
 
 class MyThread extends Thread{
     public void run(){
-        for (int i = 0; i <1000 ; i++) {
+        while (true){
+            System.out.println("Hello!");
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Hello My Thread!" + i);
         }
 
     }
