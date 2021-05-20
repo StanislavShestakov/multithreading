ef00b3456b32a747e1aa18d3ec9d46da1d54522f
admin <w3orks@mail.ru>
2021-05-20 13:21:44 +0300

    sleep, awake

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-//        MyThread myThread = new MyThread();
-//        myThread.start();
-//        MyThread myThread2 = new MyThread();
-//        myThread2.start();
-//
-        System.out.println("Im Going to sleep!");
-        Thread.sleep(3000);
-        System.out.println("Im awake!");
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        MyThread myThread2 = new MyThread();
+        myThread2.start();
+
+
     }
 }
 
 class MyThread extends Thread{
     public void run(){
         for (int i = 0; i <1000 ; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Hello My Thread!" + i);
         }
 
