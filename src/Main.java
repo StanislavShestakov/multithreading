e144bd943276d748aef2a39a137b772c5604b01d
admin <w3orks@mail.ru>
2021-05-20 13:26:06 +0300

    runnable

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        MyThread myThread = new MyThread();
-        myThread.start();
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//
+//        MyThread myThread2 = new MyThread();
+//        myThread2.start();
 
-        MyThread myThread2 = new MyThread();
-        myThread2.start();
+        Thread thread = new Thread(new Runner());
+        thread.start();
+        System.out.println("Hello from main thread!");
+    }
+}
 
-
+class Runner implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i <1000 ; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello My Thread!" + i);
+        }
     }
 }
 
