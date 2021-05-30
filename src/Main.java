6392dd7127e1a6899deb80dc5651c788b18644a8
JuniorDeveloper <w3orks@mail.ru>
2021-05-30 23:57:21 +0300

    tread deamon

--- src/Main.java
+++ src/Main.java
@@ -2,55 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Thread t1 = new Runner1();
-        Thread t2 = new Runner2();
+      Thread t1 = new Thread(new Worker());
+      t1.setDaemon(true);
 
-
-        t1.start();
-        t2.start();
-
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Finish");
+        System.out.println(t1.isDaemon());
     }
 }
 
-class Runner1 extends Thread{
+class Worker implements Runnable{
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Runner1:" + i);
-        }
+        System.out.println("Hello from a given thread....");
     }
 }
-
-class Runner2 extends Thread{
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 10; i++) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Runner2:" + i);
-        }
-    }
-}
-
-
-
 
 
