bb763936681ac9c144ce3f800b49bf1af0e5f599
admin <w3orks@mail.ru>
2021-05-31 14:55:50 +0300

    without synchronize

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,49 @@
 public class Main {
 
+    public  static int counter;
+
+    public static  void process(){
+
+        Thread t1 = new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    counter++;
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    counter++;
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("The counter is: " + counter);
+
+    }
+
     public static void main(String[] args) {
-
-      Thread t1 = new Thread(new DaemonWorker());
-      Thread t2 = new Thread(new NormalWorker());
-      t1.setDaemon(true);
-
-      t1.start();
-      t2.start();
+        process();
     }
+
+
 }
 
-class DaemonWorker implements Runnable{
-
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Daemon thread is worked....");
-        }
-    }
-}
-
-class NormalWorker implements Runnable{
-
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Normal thread finishes execution...");
-        }
-    }
-}
 
 
