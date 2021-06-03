69546737a77b305f81894f8cdb64920eca430f51
admin <w3orks@mail.ru>
2021-06-03 16:12:51 +0300

    demostrating working working without synchronization

--- src/Main.java
+++ src/Main.java
@@ -1,85 +1,49 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
+    private static int counter = 0;
+    private static Lock lock = new ReentrantLock();
+
+    public static void increment(){
+        counter++;
+    }
 
     public static void main(String[] args) {
-        Processor process = new Processor();
-        Thread t1 = new Thread(new Runnable() {
+
+        Thread t1 = new Thread(new Runnable(){
             @Override
             public void run() {
-                try {
-                    process.producer();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                increment();
             }
         });
-        Thread t2 = new Thread(new Runnable() {
+
+        Thread t2 = new Thread(new Runnable(){
             @Override
             public void run() {
-                try {
-                    process.consumer();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                increment();
             }
         });
+
         t1.start();
         t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        //whiout sunchronization sametime is 1, sometime 2
+        System.out.println("Counter is: " + counter);
+
     }
 
 }
 
-class Processor {
-
-    private List<Integer> list = new ArrayList<>();
-    private static final int UPPER_LIMIT = 5;
-    private static final int LOWER_LIMIT = 0;
-    private final Object lock = new Object();
-    private int value = 0;
-
-    public void producer() throws InterruptedException {
-        synchronized (lock) {
-            while (true) {
-                if (list.size() == UPPER_LIMIT) {
-                    System.out.println("Waiting for removing items...");
-                    lock.wait();
-                } else {
-                    System.out.println("Adding " + value);
-                    list.add(value);
-                    value++;
-                    //we can call the notify- because the other thread will be notified
-                    //only when it is in waiting state
-                    lock.notify();
-                }
-                Thread.sleep(500);
-            }
-
-
-        }
-    }
-
-    public void consumer() throws InterruptedException {
-        synchronized (lock) {
-            while (true) {
-                if (list.size() == LOWER_LIMIT) {
-                    System.out.println("Waiting for adding items...");
-                    value = 0;
-                    lock.wait();
-                } else {
-                    System.out.println("Removing " + list.remove(list.size()-1));
-                    lock.notify();
-                    //do further operations !!!
-                }
-                Thread.sleep(500);
-            }
-
-        }
-    }
-
-}
 
 
 
